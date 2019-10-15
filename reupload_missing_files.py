a684d64fe9e467147ad2ecff133f55ff9826914e
François Charlier <francois.charlier@redhat.com>
2019-10-15 16:37:22 +0200

    Format with Black

--- reupload_missing_files.py
+++ reupload_missing_files.py
@@ -11,8 +11,8 @@ import requests
 import sys
 
 
-pid_file = '/tmp/reupload_missing_files.lock'
-fp = open(pid_file, 'w')
+pid_file = "/tmp/reupload_missing_files.lock"
+fp = open(pid_file, "w")
 try:
     fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
 except IOError:
@@ -20,12 +20,13 @@ except IOError:
     sys.exit(1)
 
 
-parser = argparse.ArgumentParser(description='Search for missing container '
-                                 'images blobs in swift and upload them')
-parser.add_argument('--debug', action='store_true')
+parser = argparse.ArgumentParser(
+    description="Search for missing container " "images blobs in swift and upload them"
+)
+parser.add_argument("--debug", action="store_true")
 args = parser.parse_args()
 
-redis_session = redis.Redis(host='localhost', port=6379, db=0)
+redis_session = redis.Redis(host="localhost", port=6379, db=0)
 
 # Initialize and turn on debug logging
 openstack.enable_logging(debug=args.debug)
@@ -39,56 +40,59 @@ def empty_blobs():
         path = k.decode()
         v = redis_session.get(k)
         o = json.loads(v.decode())
-        if not k.decode().endswith('/data'):
+        if not k.decode().endswith("/data"):
             continue
-        if not path.startswith('files/docker/registry/v2/blobs/sha256'):
+        if not path.startswith("files/docker/registry/v2/blobs/sha256"):
             continue
-        if o['content_length']:
+        if o["content_length"]:
             continue
 
-        yield (path, path.split('/')[7])
+        yield (path, path.split("/")[7])
 
 
 def find_image(blob):
     for k in redis_session.keys():
         path = k.decode()
-        if not path.startswith('files/docker/registry/v2/repositories'):
+        if not path.startswith("files/docker/registry/v2/repositories"):
             continue
-        splitted = path.split('/')
+        splitted = path.split("/")
         if not splitted[9] == blob:
             continue
         return "%s/%s" % (splitted[5], splitted[6])
 
 
 def refresh_cache():
-    container = conn.object_store.get_container_metadata('dci_registry')
+    container = conn.object_store.get_container_metadata("dci_registry")
 
     for o in conn.object_store.objects(container):
         if redis_session.exists(o.id):
             continue
         try:
-            o = conn.object_store.get_object_metadata(o.name,
-                                                      container=container)
+            o = conn.object_store.get_object_metadata(o.name, container=container)
         except openstack.exceptions.ResourceNotFound:
             print("Cannot stat %s on Swift" % o.name)
             continue
-        #print('o.id: %s' % o.id)
+        # print('o.id: %s' % o.id)
         data = {
-                'content_length': o.content_length,
-                'name': o.name,
-                'id': o.id,
-                'is_static_large_object': o.is_static_large_object,
-                'copy_from': o.copy_from,
-                'object_manifest': o.object_manifest,
-                'multipart_manifest': o.multipart_manifest,
-                'content_type': o.content_type,
-                'last_modified_at': o.last_modified_at,
-                }
+            "content_length": o.content_length,
+            "name": o.name,
+            "id": o.id,
+            "is_static_large_object": o.is_static_large_object,
+            "copy_from": o.copy_from,
+            "object_manifest": o.object_manifest,
+            "multipart_manifest": o.multipart_manifest,
+            "content_type": o.content_type,
+            "last_modified_at": o.last_modified_at,
+        }
         redis_session.set(o.id, json.dumps(data))
+
 
 def get_blob_content_v1(image, blob):
     print("Trying to get the blob from docker-registry.engineering.redhat.com ...")
-    url = 'https://docker-registry.engineering.redhat.com/v2/%s/blobs/sha256:%s' % (image, blob) # noqa
+    url = "https://docker-registry.engineering.redhat.com/v2/%s/blobs/sha256:%s" % (
+        image,
+        blob,
+    )  # noqa
     headers = {"Authorization": "Bearer anonymous"}
     r = requests.get(url, headers=headers, verify=False)
     return r
@@ -101,16 +105,22 @@ def get_blob_content_v2(image, blob):
     image = image.replace("/", "-")
 
     token = requests.get(
-        "%s/v2/auth?scope=repository:%s/%s:pull" % (url, ns, image), verify=False).json()["token"]
+        "%s/v2/auth?scope=repository:%s/%s:pull" % (url, ns, image), verify=False
+    ).json()["token"]
     headers = {"Authorization": "Bearer %s" % token}
 
-    r = requests.get("%s/v2/%s/%s/blobs/sha256:%s" % (url, ns, image, blob), headers=headers, verify=False)
+    r = requests.get(
+        "%s/v2/%s/%s/blobs/sha256:%s" % (url, ns, image, blob),
+        headers=headers,
+        verify=False,
+    )
     return r
+
 
 def reupload_blobs():
     for path, blob in empty_blobs():
         image = find_image(blob)
-        print('Trying to reupload %s ...' % image)
+        print("Trying to reupload %s ..." % image)
         r = get_blob_content_v1(image, blob)
         digest = hashlib.sha256(r.content).hexdigest()
         if digest != blob:
@@ -123,9 +133,12 @@ def reupload_blobs():
                 continue
 
         print("Success. Now pushing the blob to Swift ...")
-        conn.object_store.upload_object(container="dci_registry", name=path, data=r.content)
+        conn.object_store.upload_object(
+            container="dci_registry", name=path, data=r.content
+        )
         redis_session.delete(path)
         print("Done.")
+
 
 if __name__ == "__main__":
     refresh_cache()
